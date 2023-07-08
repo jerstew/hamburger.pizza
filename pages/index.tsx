02d4323b3dc952bbadcd33d8076a265a8e70168e
Jeremy Stewart <jerstew@users.noreply.github.com>
2023-07-07 23:18:59 -0500

    Replace index with logo hamburber

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import iconPng from "../public/images/icon-16x16.png";
-import constructionGif from "../public/images/under_construction.gif";
+import logo from "../public/images/logo.png";
 
 export default function Home() {
   return (
@@ -18,31 +17,13 @@ export default function Home() {
         and isn't that fun and nice for everyone
          */}
       </Head>
-
       <main className={styles.main}>
-        <div className={styles.terminalCard + " terminal-card"}>
-          <header>
-            <Image
-              src={iconPng}
-              className={styles.iconPng}
-              alt="Hamburger pizza icon"
-              width={16}
-              height={16}
-            />
-            <p className={styles.title}>Hamburger.pizza</p>
-          </header>
-          <div>
-            <p>Website coming soon</p>
-          </div>
-        </div>
         <Image
-          src={constructionGif}
-          className={styles.homeGif}
-          alt="Animated gif depicting a construction site"
-          width={177}
-          height={175}
-          // blurDataURL="data:..." automatically provided
-          // placeholder="blur" // Optional blur-up while loading
+          src={logo}
+          className={styles.logoPng}
+          alt="Hamburger pizza icon"
+          width={120}
+          height={120}
         />
       </main>
     </div>
